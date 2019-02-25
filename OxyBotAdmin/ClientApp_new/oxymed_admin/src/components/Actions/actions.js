60d1858f972201e4f8078625d640effbbba4f4dc
timur <timur.gazaleyev@gmail.com>
2019-02-25 18:18:57 +0500

    Some changes in action module component

--- OxyBotAdmin/ClientApp_new/oxymed_admin/src/components/Actions/actions.js
+++ OxyBotAdmin/ClientApp_new/oxymed_admin/src/components/Actions/actions.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import usersService from "../../services/usersService";
+import actionService from "../../services/actionService";
 import NavBar from "../Header/Header.vue";
 import Loading from "../../views/Loading.vue";
 
@@ -70,7 +70,7 @@ export default {
             try {
                 this.isLoading = true;
 
-                const responseFromServer = await usersService.getUsers(beginPage, endPage);
+                const responseFromServer = await actionService.getActions(beginPage, endPage);
                 
                 if (responseFromServer && responseFromServer.isSuccessfully) {
                     this.tableData.items = responseFromServer.data.botUsers;
@@ -89,17 +89,35 @@ export default {
             }
             this.isLoading = false;
         },
-        saveAction: async function(){
+        async saveAction(){
             try {
                 
-                if (!this.selectedAction && !this.selectedAction.actionId)
+                if (!this.selectedAction)
                     throw new Error("Данные по акции пусты!");
 
-                const resultFromServer = await usersService.sendMessageToUser(this.messageForSend, this.selectedAction.chatId);    
+                if (!this.selectedAction.nameOfAction)
+                    throw new Error("Необходимо указать название акции!");
+                
+                if (!this.selectedAction.advertisingText)
+                    throw new Error("Необходимо указать описание акции!");
+                
+                if (!this.selectedAction.commandText || !this.selectedAction.commandText.match(/^\/[a-zA-Z_0-9]+/gm))
+                    throw new Error("Заполните правильно поле \"Команда в телеграме\"");  
+
+                if (!this.selectedAction.dateBegin || !this.selectedAction.dateEnd)
+                    throw new Error("Укажите дату начала и дату окончания акции!");
+            
+                let resultFromServer = null;
+            
+                if (this.selectedAction.actionId <= 0)
+                    resultFromServer = await actionService.saveNewAction(this.selectedAction);  
+                else 
+                    resultFromServer = await actionService.updateAction(this.selectedAction);          
+                  
                 if (resultFromServer.isSuccessfully) {
                     this.alertMessage = resultFromServer.message ? resultFromServer.message : "Данные по акции успешно сохранены!";
                     this.showSuccessAlert = true;
-                }else{
+                } else {
                     this.alertMessage = resultFromServer.message ? resultFromServer.message : "Данные не сохранены!";
                     this.showDangerAlert = true;
                 }
--- OxyBotAdmin/ClientApp_new/oxymed_admin/src/services/actionService.js
+++ OxyBotAdmin/ClientApp_new/oxymed_admin/src/services/actionService.js
@@ -1,20 +1,99 @@
 import Vue from "vue";
+const responseFromServer = {
+    data: [],
+    message: "",
+    isSuccessfully: false
+}
 
 export default {
 
-    getActions(beginPage, endPage, url=""){
+    async getActions(beginPage, endPage, url = "/api/advertising?beginPage=") {
         try {
-            
+
+            if (!beginPage || !endPage)
+                throw new Error("Couldn't load users because begin or end page not specified!");
+
+            if (!url)
+                throw new Error("Could't get users data because url parameter not specified!");
+
+            url = url + beginPage + "&endPage=" + endPage;
+            const response = await Vue.axios.get(url);
+
+            if (response && response.data) {
+                responseFromServer.data = response.data ? response.data : [];
+                responseFromServer.message = response.data.message ? response.data.message : "200. Данные успешно получены!"
+                responseFromServer.isSuccessfully = true;
+            } else {
+                responseFromServer.data = [];
+                responseFromServer.message = response.data.message ? response.data.message : "Данные не получены!"
+                responseFromServer.isSuccessfully = false;
+            }
+
         } catch (error) {
-            
+            responseFromServer.data = [];
+            if (process.env.VUE_APP_IS_DEV)
+                responseFromServer.message = JSON.stringify(error);
+            else
+                responseFromServer.message = error.response.data ? error.response.data : "Данные не получены!"
+            responseFromServer.isSuccessfully = false;
         }
+
+        return responseFromServer;
     },
 
-    saveAction(actionData, url=""){
+    async saveNewAction(actionData, url = "/api/advertising") {
         try {
-            
+
+            if (!actionData)
+                throw new Error("Couldn't save action! Action data is null or undefined!");
+
+            if (!url)
+                throw new Error("Could't save action data because url parameter not specified!");
+
+            const response = await Vue.axios.post(url, actionData);
+            responseFromServer.isSuccessfully = true;
+            responseFromServer.message = "Данные успешно сохранены!";
+
+            if (response && response.data)
+                responseFromServer.message = response.data.message ? response.data.message : responseFromServer.message;
+
         } catch (error) {
+
+            if (process.env.VUE_APP_IS_DEV)
+                responseFromServer.message = JSON.stringify(error);
+            else
+                responseFromServer.message = error.response.data ? error.response.data : "Данные не сохранены!"
             
+            responseFromServer.isSuccessfully = false;
+        }
+
+        return responseFromServer;
+    },
+
+    async updateAction(actionData, url = "/api/advertising") {
+        try {
+            if (!actionData)
+                throw new Error("Couldn't save action! Action data is null or undefined!");
+
+            if (!url)
+                throw new Error("Couldn't save action data because url parameter not specified!");
+
+            const response = Vue.axios.put(url, actionData);
+            responseFromServer.isSuccessfully = true;
+            responseFromServer.message = "Данные успешно обновлены!";
+
+            if (response && response.data)
+                responseFromServer.message = response.data.message ? response.data.message : responseFromServer.message;
+
+        } catch (error) {
+
+            if (process.env.VUE_APP_IS_DEV)
+                responseFromServer.message = JSON.stringify(error);
+            else
+                responseFromServer.message = error.response.data ? error.response.data : "Данные не обновлены!"
+            
+            responseFromServer.isSuccessfully = false;
         }
     }
+
 }
